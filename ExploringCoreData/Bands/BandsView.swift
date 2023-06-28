49deafd3c942a03d59ae2ec86ff9093f9907d9f6
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-06-28 17:46:27 -0300

    bandsView emptyState

--- ExploringCoreData/Bands/BandsView.swift
+++ ExploringCoreData/Bands/BandsView.swift
@@ -15,39 +15,45 @@ struct BandsView: View {
         self.viewModel = viewModel
     }
     
-    var body: some View {
-        NavigationStack {
-            List{
-                ForEach(viewModel.bands) { band in
-                    NavigationLink {
-                        SongsView(viewModel: SongsViewModel(band: band))
-                        
-                    } label: {
-                        Text(band.unwrappedName)
-                            .contextMenu {
-                                Button("Delete", role: .destructive) {
-                                    withAnimation(.easeIn(duration: 1)) {
-                                        viewModel.deleteBand(for: band)
-                                    }
-                                }
-                                
-                            }
-                        
-                    }
-                }
-                .onDelete { offsets in
-                    withAnimation() {
-                        offsets.forEach { index in
-                            viewModel.deleteBand(atIndex: index)
-                        }
-                    }
-                }
+    private var emptyView: some View {
+        
+        VStack(spacing: 40) {
+            Image(systemName: "music.mic.circle.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Text("1. Nessa tela você pode visualizar as bandas.")
+                Text("2. Utilize o botão superior para estar adicionando novas bandas para sua lista.")
+                Text("3. Para adicionar músicas para essa banda, basta clicar nela.")
+                Text("4. Caso deseje, você pode exluir uma banda a deslizando para a esquerda.")
+                
             }
             
-            .navigationTitle("Bands")
+            .font(.headline)
+            
+        }
+        .foregroundColor(.secondary)
+        .padding(.horizontal, 50)
+        
+        
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                switch viewModel.state {
+                case .content:
+                    bandList
+                case .empty:
+                    emptyView
+                }
+                
+            }
+            .navigationTitle("Bandas")
             .toolbar {
                 Button {
-                    
                     viewModel.isPresentingSheet = true
                 } label: {
                     Image(systemName: "plus")
@@ -56,12 +62,40 @@ struct BandsView: View {
             }
             
             .sheet(isPresented: $viewModel.isPresentingSheet) {
-                //                sheetView
+                sheetView
             }
-            
         }
     }
     
+    
+    private var bandList: some View {
+        List{
+            ForEach(viewModel.bands) { band in
+                NavigationLink {
+                    SongsView(viewModel: SongsViewModel(band: band))
+                    
+                } label: {
+                    Text(band.unwrappedName)
+                        .contextMenu {
+                            Button("Delete", role: .destructive) {
+                                withAnimation(.easeIn(duration: 1)) {
+                                    viewModel.deleteBand(for: band)
+                                }
+                            }
+                            
+                        }
+                    
+                }
+            }
+            .onDelete { offsets in
+                withAnimation() {
+                    offsets.forEach { index in
+                        viewModel.deleteBand(atIndex: index)
+                    }
+                }
+            }
+        }
+    }
     
     private var sheetView: some View {
         
@@ -94,7 +128,7 @@ struct BandsView: View {
             } label: {
                 Image(systemName: "plus")
             }
-
+            
         }
         
     }
